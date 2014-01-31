ea17f5a71fa569b3f5ed04c42f803cf45e218ac4
Patrick Ewing <patrick.henry.ewing@gmail.com>
2014-01-30 20:47:49 -0800

    Workin pretty fine

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "google_drive"
+gem "erubis"
 gem "shotgun"
--- app.rb
+++ app.rb
@@ -1,22 +1,38 @@
 # require "bundler"
 require 'google_drive'
+require 'erubis'
 require 'sinatra'
+set :erb, :escape_html => true
+
                                                               name, sec = "patrick@camposanto.com", "c4mp3s1n0"
-# Logs in.
 # You can also use OAuth. See document of
 # GoogleDrive.login_with_oauth for details.
 session = GoogleDrive.login(name, sec)
-index = File.read 'index.html'
 WS = session.spreadsheet_by_key("0AoAp79Ob8GbIdDVtN1VQQ0dnQi1FQWh1ZlhKUXJURXc").worksheets[0]
 
+class Line
+  attr_accessor :id, :text, :character
+end
+
+def get_lines
+  WS.reload
+  WS.rows[1..-1].collect do |row|
+    line = Line.new
+    line.id = row[0]
+    line.character = row[1]
+    line.text = row[2]
+    line
+  end
+end
+
 def add_row(data)
-  dialog_id = data["id"]
+  line_id = data["id"]
   row_to_update = -1;
 
   for row in 2..WS.num_rows
     row_id = WS[row, 1]
     puts row, row_id
-    if row_id == dialog_id
+    if row_id == line_id
       puts "We have a winner", row_id
       row_to_update = row
       break
@@ -24,14 +40,13 @@ def add_row(data)
   end
 
   row_to_update = WS.num_rows +  1 if row_to_update < 0 #this is a new row
-  WS[row_to_update, 1] = dialog_id
-  WS[row_to_update, 3] = "Sinbad"
-  WS[row_to_update, 3] = data["heard"]
+  WS[row_to_update, 1] = line_id
+  WS[row_to_update, 3] = data["text"]
   WS.save
 end
 
 get '/' do
-  index
+  erb :index, locals: {lines: get_lines}
 end
 
 get '/rows' do
@@ -50,8 +65,3 @@ post '/update' do
   add_row params
 end
 
-# Yet another way to do so.
-# p ws.rows  #==> [["fuga", ""], ["foo", "bar]]
-
-# Reloads the worksheet to get changes by other clients.
-# ws.reload
