0be17ecd45b065ef6c5b43302e8c5ed59d20d72e
Patrick Ewing <patrick.henry.ewing@gmail.com>
2014-01-30 20:24:26 -0800

    wow such sync

--- app.rb
+++ app.rb
@@ -7,11 +7,27 @@ require 'sinatra'
 # GoogleDrive.login_with_oauth for details.
 session = GoogleDrive.login(name, sec)
 index = File.read 'index.html'
-ws = session.spreadsheet_by_key("0AoAp79Ob8GbIdDVtN1VQQ0dnQi1FQWh1ZlhKUXJURXc").worksheets[0]
+WS = session.spreadsheet_by_key("0AoAp79Ob8GbIdDVtN1VQQ0dnQi1FQWh1ZlhKUXJURXc").worksheets[0]
 
 def add_row(data)
-  ws.reload
-  puts ws.rows
+  dialog_id = data["id"]
+  row_to_update = -1;
+
+  for row in 2..WS.num_rows
+    row_id = WS[row, 1]
+    puts row, row_id
+    if row_id == dialog_id
+      puts "We have a winner", row_id
+      row_to_update = row
+      break
+    end
+  end
+
+  row_to_update = WS.num_rows +  1 if row_to_update < 0 #this is a new row
+  WS[row_to_update, 1] = dialog_id
+  WS[row_to_update, 3] = "Sinbad"
+  WS[row_to_update, 3] = data["heard"]
+  WS.save
 end
 
 get '/' do
@@ -19,11 +35,11 @@ get '/' do
 end
 
 get '/rows' do
-  ws.reload
+  WS.reload
   s = ""
-  for row in 1..ws.num_rows
-    for col in 1..ws.num_cols
-      s << ws[row, col] + "\n"
+  for row in 1..WS.num_rows
+    for col in 1..WS.num_cols
+      s << WS[row, col] + "\n"
     end
   end
   s
@@ -31,10 +47,7 @@ end
 # Dumps all cells.
 
 post '/update' do
-  puts params["id"]
-  puts params["heard"]
-  ws[1, 3] = params["heard"]
-  ws.save
+  add_row params
 end
 
 # Yet another way to do so.
