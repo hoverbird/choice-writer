a540992ae0b9749bba8896b4e1af6120b3b37ad4
Patrick Ewing <patrick.henry.ewing@gmail.com>
2014-04-07 14:49:47 -0700

    Serve static assets (for now)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,11 @@ ChoiceWriter::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
+
+  # Adding this for Heroku a la https://github.com/heroku/rails_serve_static_assets
+  config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = nil
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
