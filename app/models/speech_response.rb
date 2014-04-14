f3090023de8b96e182bccf2338b4488500c1f506
Patrick Ewing <patrick.henry.ewing@gmail.com>
2014-04-14 15:25:24 -0700

    Flatten SpeechResponse hash out a bit

--- app/models/speech_response.rb
+++ app/models/speech_response.rb
@@ -24,21 +24,18 @@ class SpeechResponse < Response
 
   def to_web_hash
     unity_hash = {"Type" => self.class.name}
-    speech_hash = {}
 
-    speech_hash["Caption"] = clean_text
+    unity_hash["Caption"] = clean_text
 
     # Always trigger a finish event, even if there is not currently another response
-    speech_hash["OnFinishEvent"] = on_finish_event
-    speech_hash["OnFinishEventDelay"] = on_finish_event_delay if on_finish_event_delay
+    unity_hash["OnFinishEvent"] = on_finish_event
+    unity_hash["OnFinishEventDelay"] = on_finish_event_delay if on_finish_event_delay
 
-    speech_hash["HackAudioDuration"] = hack_audio_duration if hack_audio_duration
-    speech_hash["MinimumDuration"] = minimum_duration if minimum_duration
-    speech_hash["AllowQueueing"] = allow_queueing unless allow_queueing.nil?
-    speech_hash["AudioClipPath"] = audio_clip_path if audio_clip_path
+    unity_hash["HackAudioDuration"] = hack_audio_duration if hack_audio_duration
+    unity_hash["MinimumDuration"] = minimum_duration if minimum_duration
+    unity_hash["AllowQueueing"] = allow_queueing unless allow_queueing.nil?
+    unity_hash["AudioClipPath"] = audio_clip_path if audio_clip_path
 
-    # Attach the Speech Hash to the main Speech Response Hash
-    unity_hash["SpeechToPlay"] = speech_hash
     unity_hash
   end
 end
